7e0d4831aafb8f42aa09361d7c6e49a0b4dc3111
Ye htut khaung <yehtutkhaung@ucstgi.edu.mm>
2022-06-28 16:10:34 +0630

    FIrst Commit

---
+++ client/hr/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "information">
+      <label>Name :</label>
+     <input type="text" />
+     <label>Age :</label>
+     <input type="text" />
+     <label>Country :</label>
+     <input type="text" />
+     <label>Position :</label>
+     <input type="text" />
+     <label>Wage (year):</label>
+     <input type="number" />
+     <button>Add Exployee</button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
